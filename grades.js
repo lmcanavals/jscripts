803e86a4499126a63c25a35e1b37aff3d16afbb7
Luis M Canaval S <public02@canaval.org>
2023-07-05 22:57:50 -0500

    new bb, individual and group

--- grades.js
+++ grades.js
@@ -1,4 +1,72 @@
-// run on blackboard
+// we need to get the codes for this students
+// This first!
+const table = document.querySelectorAll("span table")[12];
+const trs = table.querySelectorAll("tr");
+
+const name2code = {};
+for (let i = 3; i < trs.length - 1; ++i) {
+  const name = trs[i]
+    .querySelector("td:nth-child(2)")
+    .innerText
+    .trim()
+    .toLowerCase()
+    .split(", ")
+    .reverse()
+    .join(" ");
+  const code = trs[i]
+    .querySelector("td:nth-child(1)")
+    .innerText
+    .trim()
+    .toLowerCase();
+  name2code[name] = code;
+}
+console.log(name2code);
+
+// run on new blackboard individual evaluation
+
+const rows = document.querySelectorAll(".individual-submission-row");
+const mux = {};
+for (const row of rows) {
+  if (row.querySelector(".status-nothing-to-grade") === null) {
+    const name = row.querySelector("bdi").innerText.toLowerCase();
+    const grade = row.querySelectorAll("input")[1].value;
+    if (!(name in name2code)) {
+      console.log("ERROR ERRRO, name is written different");
+      continue;
+    }
+    mux[name2code[name]] = parseFloat(grade);
+  }
+}
+console.log(mux);
+
+// run on new blackboard group evaluation
+let groupdivs = document.querySelectorAll("form > div");
+for (let i = 1; i < groupdivs.length; ++i) {
+  const btn = groupdivs[i].querySelectorAll("button")[0];
+  if (btn !== undefined) {
+    btn.click();
+  } else {
+    console.log("undefined button");
+  }
+}
+await new Promise((r) => setTimeout(r, 2000));
+groupdivs = document.querySelectorAll("form > div");
+const mex = {};
+for (let i = 1; i < groupdivs.length; ++i) {
+  const rows = groupdivs[i].querySelectorAll(".member-row");
+  for (const row of rows) {
+    const name = row.querySelector("bdi").innerText.toLowerCase();
+    const grade = row.querySelector("input").value;
+    if (!(name in name2code)) {
+      console.log("ERROR ERRRO, name is written different");
+      continue;
+    }
+    mex[name2code[name]] = parseFloat(grade);
+  }
+}
+console.log(mex);
+
+// run on classic blackboard
 
 const codecolumn = 3;
 const gradecolumn = 6;
@@ -37,5 +105,7 @@ for (const inp of inputs) {
   const code = inp.id.substring(7, 16).toLowerCase();
   if (code in mix && mix[code] !== null) {
     inp.value = mix[code];
+  } else {
+    console.log(`ERROR ERROR ${code}`);
   }
 }
